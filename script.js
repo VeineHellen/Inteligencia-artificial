797e3378187bbd93949f2e13513624f9ed005de9
Hellen Veine <141366243+VeineHellen@users.noreply.github.com>
2025-05-28 13:28:20 +0000

    aaaa

---
+++ script.js
@@ -0,0 +1,32 @@
+const caixaPrincipal = document.querySelector(".caixa-principal")
+const caixaPerguntas = document.querySelector(".caixa-perguntas")
+const caixaAlternativas = document.querySelector(".caixa-alternativas")
+const caixaResultado = document.querySelector(".caixa-resultado")
+const textoResultado = document.querySelector(".texto-resultado")
+const perguntas = [
+    {
+        enunciado: "Pergunta1",
+        alternativas: [
+        {
+            texto: "alternativa1",
+            afirmacao: "afirmacao1"
+        },
+        {
+            texto: "alternativa2",
+            afirmacao: "afirmacao2"
+        },
+        ]
+{
+    enunciado:"Pergunta2",
+    alternativas: [ 
+        {
+            texto: "alternativa1",
+            afirmacao: "afirmacao1"
+        },
+        {
+            texto: "alternativa2",
+            afirmacao: "afirmacao2"
+        },
+    ]
+}
+]
