60f5b5d79beda603ab0dc51366c379abe31b1cf2
Hellen Veine <141366243+VeineHellen@users.noreply.github.com>
2025-06-11 14:14:11 +0000

    aa

--- script.js
+++ script.js
@@ -4,46 +4,47 @@ const caixaAlternativas = document.querySelector(".caixa-alternativas")
 const caixaResultado = document.querySelector(".caixa-resultado")
 const textoResultado = document.querySelector(".texto-resultado")
 const perguntas = [
-    {
+     {
         enunciado: "Como a escola pode usar a tecnologia para criar um ambiente seguro, combatendo a homofobia de forma proativa e acolhendo estudantes LGBTQIA+?",
         alternativas: [
-        {
-            texto: "A escola pode implementar plataformas digitais para denúncias anônimas de homofobia e desenvolver aplicativos educativos sobre diversidade.",
-            afirmacao: "Ferramentas digitais seguras e informativas fortalecem o suporte a grupos minoritários."
-        },
-        {
-            texto: "Usar realidade virtual (RV) ou jogos educativos pode simular preconceito, desenvolvendo empatia e capacitando os alunos a intervir.",
-            afirmacao: "Experiências imersivas aprimoram a compreensão e a sensibilidade social."
-        },
-    ]
+            {
+                texto: "A escola pode implementar plataformas digitais para denúncias anônimas de homofobia e desenvolver aplicativos educativos sobre diversidade.",
+                afirmacao: "Ferramentas digitais seguras e informativas fortalecem o suporte a grupos minoritários."
+               
+            },
+            {
+                texto: "Usar realidade virtual (RV) ou jogos educativos pode simular preconceito, desenvolvendo empatia e capacitando os alunos a intervir.",
+                afirmacao: "Experiências imersivas aprimoram a compreensão e a sensibilidade social."
+            },
+        ]
     },
-{
-    enunciado:" tecnologia pode espalhar homofobia. Qual o papel da escola em mitigar isso e promover o uso responsável entre os jovens?",
-    alternativas: [ 
-        {
-            texto: "Redes sociais propagam cyberbullying. A escola deve educar sobre cidadania digital e pensamento crítico para identificar e combater esses conteúdos.",
-            afirmacao: "A educação digital é crucial para navegar em ambientes online complexos."
-        },
-        {
-            texto: "Algoritmos criam "bolhas de filtro". A escola precisa ensinar a questionar informações online e buscar fontes diversas para desconstruir narrativas homofóbicas.",
-            afirmacao: "Compreender algoritmos e diversidade de informação evita a polarização."
-        },
-    ]
-},
-{
-    enunciado:"Pergunta3",
-    alternativas: [ 
-        {
-            texto: "alternativa1",
-            afirmacao: "afirmacao1"
-        },
-        {
-            texto: "alternativa2",
-            afirmacao: "afirmacao2"
-        },
-    ]
-},
-]
+    {
+        enunciado: "Qual o papel da escola em mitigar isso e promover o uso responsável entre os jovens?",
+        alternativas: [
+            {
+                texto: "Redes sociais propagam cyberbullying. A escola deve educar sobre cidadania digital e pensamento crítico para identificar e combater esses conteúdos.",
+                afirmacao: "A educação digital é crucial para navegar em ambientes online complexos."
+            },
+            {
+                texto: "Algoritmos criam "bolhas de filtro". A escola precisa ensinar a questionar informações online e buscar fontes diversas para desconstruir narrativas homofóbicas.",
+                afirmacao: "Compreender algoritmos e diversidade de informação evita a polarização."
+            },
+        ]
+    },
+    {
+        enunciado: "Pergunta3",
+        alternativas: [
+            {
+                texto: "alternativa1",
+                afirmacao: "afirmacao1"
+            },
+            {
+                texto: "alternativa2",
+                afirmacao: "afirmacao2"
+            },
+        ]
+    },
+];
 
 let atual = 0;
 let perguntaAtual;
